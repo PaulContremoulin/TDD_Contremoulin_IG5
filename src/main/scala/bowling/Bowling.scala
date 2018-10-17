14732377f59857ab19dd8d6a2294afcd69ab78e9
PaulContremoulin <paul.contremoulin@etu.umontpellier.fr>
2018-10-17 21:23:59 +0200

    Rendu Contremoulin Paul

--- src/main/scala/bowling/Bowling.scala
+++ src/main/scala/bowling/Bowling.scala
@@ -1,5 +1,65 @@
 package bowling
 
-object Bowling {
+/**
+  * @author Contremoulin Paul - IG5
+  */
+
+case class Bowling(score: Int, frames: Int, rolls : Int, accruedScore: Int){
+
+  def isFinished : Boolean = thirdOfLastFramePerformed || noBonusOnSecondOfLastFrame
+
+  def addPins(number: Int): Bowling = {
+    if(inFirstRoll()) {
+      if(isStrike(number)) {
+        if(isLastFrame) {
+          if(consecutiveStrike) copy(bonus(number), frames, switchRoll, decrementStrikeBonus + number)
+          else copy(score, frames, switchRoll, accruedScore + number)
+        }
+        else if(consecutiveStrike) copy(bonus(number), incrementFrame, rolls, decrementStrikeBonus + number)
+        else copy(score, incrementFrame, rolls, accruedScore + number)
+      }
+      else if(consecutiveStrike) copy(bonus(number), frames, switchRoll, decrementStrikeBonus + number)
+      else if(consecutiveSpare) copy(bonus(number), frames, switchRoll, number)
+      else copy(score, frames, switchRoll, accruedScore + number)
+    }
+    else {
+      if(isStrike(number)) copy(bonus(number), incrementFrame, switchRoll, decrementStrikeBonus + number)
+      else if(isSpare(number)) copy(score, incrementFrame, switchRoll, number + accruedScore )
+      else copy(score + number + accruedScore, incrementFrame, switchRoll, 0)
+    }
+  }
+
+  private def noBonusOnSecondOfLastFrame : Boolean = numberOfRolls == 20 && accruedScore == 0
+
+  private def thirdOfLastFramePerformed : Boolean = numberOfRolls >= 21
+
+  private def inFirstRoll() : Boolean = rolls == 0
+
+  private def isStrike(number: Int) : Boolean = number == 10
+
+  private def isSpare(number: Int) : Boolean = number + accruedScore == 10
+
+  private def consecutiveStrike : Boolean = accruedScore == 20
+
+  private def consecutiveSpare : Boolean = accruedScore == 10
+
+  private def incrementFrame : Int = frames+1
+
+  private def switchRoll : Int = if(inFirstRoll()) 1 else 0
+
+  private def numberOfRolls : Int = frames * 2 +rolls
+
+  private def decrementStrikeBonus : Int = accruedScore - 10
+
+  private def isLastFrame : Boolean = frames == 9
+
+  private def bonus(number : Int) : Int = score + accruedScore + number
 
 }
+
+object Bowling {
+
+  def apply() = new Bowling(0, 0, 0, accruedScore = 0)
+
+}
+
--- src/test/scala/bowling/BowlingSpec.scala
+++ src/test/scala/bowling/BowlingSpec.scala
@@ -2,6 +2,93 @@ package bowling
 
 import org.scalatest.{FunSpec, Matchers}
 
+import scala.annotation.tailrec
+
+
+/**
+  * @author Contremoulin Paul - IG5
+  */
+
 class BowlingSpec extends FunSpec with Matchers {
 
+
+  implicit def intTimes(i: Int) = new {
+    def times(fn: => Bowling) = (1 to i) foreach (x => fn)
+  }
+
+  implicit def bowlingAction(game: Bowling) = new {
+
+    def withSpare = {
+      game.addPins(7).addPins(3)
+    }
+
+    def withPins(i : Int) = {
+      game.addPins(i)
+    }
+
+    def withStrike = {
+      game.addPins(10)
+    }
+
+  }
+
+  describe("Bowling score"){
+
+    it("should be 0 if 0 pin on 10 frames") {
+      aBowlingWith(0) should have (
+        'score(0)
+      )
+    }
+
+    it("should be 20 if 1 pin on 10 frames") {
+      aBowlingWith(1) should have (
+        'score(20)
+      )
+    }
+
+    it("should be 300 if 10 pin (strike) on 10 frames") {
+      aBowlingWith(10) should have (
+        'score(300)
+      )
+    }
+
+    it("should be 150 if 5 pins then 5 pins (spare) on 10 frames") {
+
+      aGameWithSpare5() should have (
+        'score(150)
+      )
+    }
+
+    it("should be 170 if 7 pins then 3 pins (spare) on 10 frames") {
+      aGameWithSpare(7,3) should have (
+        'score(170)
+      )
+    }
+
+    it("should be 130 if 3 pins then  pins (spare) on 10 frames") {
+      aGameWithSpare(3,7) should have (
+        'score(130)
+      )
+    }
+
+  }
+
+  def aGame : Bowling = Bowling()
+
+  def aGameWithSpare5() : Bowling = aBowlingWith(5)
+
+  @tailrec
+  private def aGameWithSpare(firstPins: Int = 7, secondPins: Int, game: Bowling = Bowling()) : Bowling = {
+    if(game.isFinished) game
+    else aGameWithSpare(secondPins, firstPins, game.addPins(firstPins))
+  }
+
+  @tailrec
+  private def aBowlingWith(numberPins: Int, game: Bowling = Bowling()) : Bowling = {
+    if(game.isFinished) game
+    else aBowlingWith(numberPins, game.addPins(numberPins))
+    //(1 to 2*10).foldLeft(game.addPins(numberPins)) ( (a, _) => a.addPins(numberPins) )
+  }
+
 }
+
